1966f686b103a8b182c7ad41405d340e58eae7df
abhixshake <abhisheks10901@gmail.com>
2024-07-19 11:11:12 +0530

    style: change component alignment

--- qrcodegenApp/src/components/Qrcode.jsx
+++ qrcodegenApp/src/components/Qrcode.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import QRCode from "react-qr-code"
+import "../../src/App.css"
 
 function Qrcode() {
     const [qrCode, setQrCode] = useState('');
@@ -11,12 +12,12 @@ function Qrcode() {
 
 
     return (
-        < >
+        <div className='qrcontainer'>
             <h1>QR Code Generator</h1>
             <input onChange={(e)=>setInput(e.target.value)} type="text" name="qrcode" placeholder="Enter text to generate QR" value={input} />
-            <button disabled={input && input.trim() !== "" ? false : true} onClick={handleGenerateQrCode}>Generate</button>
+            <button style={{backgroundColor: "green"}} disabled={input && input.trim() !== "" ? false : true} onClick={handleGenerateQrCode}>Generate</button>
             <QRCode id="qr-code-value" value={qrCode} size={300} bgcolor="#fff"></QRCode>
-        </>
+        </div>
 
 
     );
